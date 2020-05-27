97a4b4935fab5572e7b8808b3da8497388265f24
ipashkovich <bagrationi2009@rambler.ru>
2020-05-27 23:26:20 +0300

    Modul5_1_Task1-Task5_8

--- DZ/src/Modul051/Mod5i1Task1.java
+++ DZ/src/Modul051/Mod5i1Task1.java
@@ -1,6 +1,6 @@
 package Modul051;
 /*
- * ������� 1кодировка 
+ * ������� 
 ������ � ���������� ������ ������, � ����� ���� ������. 
 �������� �� ������� ������� � ���������� ���������� (���� 
 ��������� ������� � ������). � ������ ���� ���������� �� 
@@ -14,35 +14,34 @@ import java.util.regex.Pattern;
 
 public class Mod5i1Task1 {
 	public static void main (String[] Args){
-		System.out.println("������� �����");
-		Scanner scan = new Scanner(System.in);
-		String userText = scan.nextLine();//����� ������������ ьь
+    System.out.println("������� �����");
+	Scanner scan = new Scanner(System.in);
+	String userText = scan.nextLine();//����� ������������ 
 		
-		System.out.println("������� ��� ���� ������");
-		String userSymb = scan.next();//������ ������������
-		scan.close();
+	System.out.println("������� ��� ���� ������");
+	String userSymb = scan.next();//������ ������������
+	scan.close();
 		
-		if (userSymb.length() > 1) {
-			System.out.println("�� ����� ������������ ������");
-		} else {
+	if (userSymb.length() > 1) {
+		System.out.println("�� ����� ������������ ������");
+	} else {
 
-	        Pattern pattern = Pattern.compile(userSymb);
-	        Matcher matcher = pattern.matcher(userText);
-	        int n = 0;
+	   Pattern pattern = Pattern.compile(userSymb);
+	   Matcher matcher = pattern.matcher(userText);
+	   int n = 0;
 	        
-	        if (matcher.find()){
+	   if (matcher.find()){
 	        	
-	        while (matcher.find()) {
-	        	//System.out.println(matcher.group()); // ������� ��������� �������
-	            System.out.println(matcher.start()); // ������� �������
-	            n++;
-	        	}  
-	        } else {
+	   while (matcher.find()) {
+	    //System.out.println(matcher.group()); // ������� ��������� �������
+	    System.out.println(matcher.start()); // ������� �������
+	    n++;
+	    }  
+	} else {
 	        	   System.out.println("���������� ���");
-	           }
-	     }
-	   }
-
-	}
+	        }
+        }
+    }
+}
 
 
