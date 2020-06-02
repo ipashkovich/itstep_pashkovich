8719c643d135474b0a2ef80ecb05753cea867912
ipashkovich <64708326+ipashkovich@users.noreply.github.com>
2020-06-02 23:54:50 +0300

    Update Mod5i1Task10.java

--- DZ/src/Modul051/Mod5i1Task10.java
+++ DZ/src/Modul051/Mod5i1Task10.java
@@ -8,9 +8,9 @@ package Modul051;
  Пример вывода для строки testing:
  */
 public class Mod5i1Task10 {
-	public static void main(String[] Args) {
-		String str = new String("testing");
-		// ВАРИАНТ 1
+    public static void main(String[] Args) {
+	String str = new String("testing");
+	// ВАРИАНТ 1
 		/*
 		 * for (int i = 1; i <= str.length(); i++){ 
 		 * System.out.printf("%50." + i + "s%n", str); }
@@ -20,21 +20,20 @@ public class Mod5i1Task10 {
 		 * System.out.printf("%" + otstup + "s%n", str.substring(i)); }
 		 */
 
-		// ВАРИАНТ 2
-		int k = 1;
-		do {
-			switch (k) {
-			case (1):
-				for (int i = 1; i <= str.length(); i++) {
-					System.out.printf("%50." + i + "s%n", str);
-				}
-			case (2):
-				for (int i = 1; i <= str.length(); i++) {
-					int otstup = 50 - i;
-					System.out.printf("%" + otstup + "s%n", str.substring(i));
-				}
-			}
-			k++;
-		} while (k < 2);
-	}
+	// ВАРИАНТ 2
+	int k = 1;
+	do {
+	    switch (k) {
+		case (1):
+		    for (int i = 1; i <= str.length(); i++) {
+			System.out.printf("%50." + i + "s%n", str);
+		    }
+		case (2):
+		     for (int i = 1; i <= str.length(); i++) {
+			int otstup = 50 - i;
+			System.out.printf("%" + otstup + "s%n", str.substring(i));
+		      }
+	    } k++;
+	} while (k < 2);
+    }
 }
