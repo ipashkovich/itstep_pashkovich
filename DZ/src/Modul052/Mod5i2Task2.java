54c90822f526dd3c78565926866ec2c216a08f67
ipashkovich <64708326+ipashkovich@users.noreply.github.com>
2020-06-02 23:48:18 +0300

    Update Mod5i2Task2.java

--- DZ/src/Modul052/Mod5i2Task2.java
+++ DZ/src/Modul052/Mod5i2Task2.java
@@ -9,18 +9,18 @@ package Modul052;
 import java.util.Arrays;
 
 public class Mod5i2Task2 {
-		public static void main(String[] Args) {
-			int[] arrRamdom = new int[30]; //создали массив на 30 элементов
+	public static void main(String[] Args) {
+		int[] arrRamdom = new int[30]; //создали массив на 30 элементов
 			
-			final int min = -70; // Минимальное число для диапазона
-			final int max = +50; // Максимальное число для диапазона
+		final int min = -70; // Минимальное число для диапазона
+		final int max = +50; // Максимальное число для диапазона
 			
-			for (int i = 0; i < 30; i++){ //заполнили массив
-				arrRamdom[i] = (int) (Math.random() * min) + max;
-			}
-			
-			Arrays.sort(arrRamdom);
-			System.out.println("Минимальный элемент" + arrRamdom[0]);
-			System.out.println("Максимальный элемент" + arrRamdom[29]);
+		for (int i = 0; i < 30; i++){ //заполнили массив
+			arrRamdom[i] = (int) (Math.random() * min) + max;
 		}
+			
+		Arrays.sort(arrRamdom);
+		System.out.println("Минимальный элемент" + arrRamdom[0]);
+		System.out.println("Максимальный элемент" + arrRamdom[29]);
+	}
 }
