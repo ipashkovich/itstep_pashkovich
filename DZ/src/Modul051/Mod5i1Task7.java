7aafd44c98725dd32eeea459c29af8901bdd0d79
ipashkovich <64708326+ipashkovich@users.noreply.github.com>
2020-06-02 23:52:32 +0300

    Update Mod5i1Task7.java

--- DZ/src/Modul051/Mod5i1Task7.java
+++ DZ/src/Modul051/Mod5i1Task7.java
@@ -12,24 +12,23 @@ package Modul051;
 import java.util.Scanner;
 
 public class Mod5i1Task7 {
-	public static void main(String[] Args) {
-		System.out.println("Введите предложение");
-		Scanner scan = new Scanner(System.in);
-		String str = scan.nextLine();
-		scan.close();
-		str = str.replaceAll("[0-9]", "_"); // цифры меняем на _
+    public static void main(String[] Args) {
+	System.out.println("Введите предложение");
+	Scanner scan = new Scanner(System.in);
+	String str = scan.nextLine();
+	scan.close();
+	str = str.replaceAll("[0-9]", "_"); // цифры меняем на _
 
-		char[] array = str.toCharArray();
-		for (int i = 0; i < array.length; i++) {
-			if (Character.isLetter(array[i])) {// (Character.isDigit(array[i]))
-												// для поиска цифр
-				if (Character.isLowerCase(array[i])) {
-					array[i] = Character.toUpperCase(array[i]);
-				} else {
-					array[i] = Character.toLowerCase(array[i]);
-				}
-			}
+	char[] array = str.toCharArray();
+	for (int i = 0; i < array.length; i++) {
+	    if (Character.isLetter(array[i])) {// (Character.isDigit(array[i]))// для поиска цифр
+		if (Character.isLowerCase(array[i])) {
+		    array[i] = Character.toUpperCase(array[i]);
+		} else {
+		    Array[i] = Character.toLowerCase(array[i]);
 		}
-		System.out.println(new String(array));
+	     }
 	}
+		System.out.println(new String(array));
+    }
 }
